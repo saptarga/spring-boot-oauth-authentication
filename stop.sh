386161f3d86893cb60915239bfb20d2b40b456c0
saptarga <saptarga@gmail.com>
2021-08-06 12:41:58 +0700

    Add Docker Configuration

--- stop.sh
+++ stop.sh
@@ -7,5 +7,5 @@ read -p "Do you want to remove created image ? (y/n) " option
 
 if [[ ${option} == y ]]
 then
-    docker image rm spring-boot-oauth-service_web-service:latest
+    docker image rm spring-boot-oauth-authentication_web-service
 fi
